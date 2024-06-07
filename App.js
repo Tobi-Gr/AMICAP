4b5cf2081f5621d766a14d645243838346608904
Android Studio <devandroid@ort>
2024-06-07 12:07:03 -0300

    componente boton

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import {StatusBar} from 'expo-status-bar';
 import {Stylesheet, Text, View} from 'react-native';
+import {Boton} from './components/Boton.tsx'
 
 export default function App()
 {
     return(
         <View style={styles.container}>
             <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto"/>
         </View>
     );
 }
