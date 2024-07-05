d4879ee21d8b6e1d573f1b5df9f9cccf120f4d96
Android Studio <devandroid@ort>
2024-07-05 09:00:26 -0300

    ejecuta

--- Amicap_Expo/app/_layout.tsx
+++ Amicap_Expo/app/_layout.tsx
@@ -13,7 +13,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    FiraSans: require('../assets/fonts/FiraSans-Regular.ttf'),
   });
 
   useEffect(() => {
