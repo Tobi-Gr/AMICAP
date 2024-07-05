1c3f522a0b871b62f1a8bd724c53d0696c34c80b
Android Studio <devandroid@ort>
2024-07-05 10:56:40 -0300

    fondo turquesa

---
+++ Amicap_Expo/components/FondoAzul.tsx
@@ -0,0 +1,26 @@
+import {View, StyleSheet, Dimensions} from 'react-native';
+import {Colores} from './../constants/Colors';
+const FondoAzul  = () => {
+    const windowWidth = Dimensions.get('window').width;
+    const windowHeight = Dimensions.get('window').height;
+    const rectangleHeight = windowHeight * 0.7;
+
+    return <View style={[styles.container, { width: windowWidth }]}>
+                <View style={[styles.rectangle, { height: rectangleHeight }]} />
+            </View>;
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-end', 
+        alignItems: 'center', 
+    },
+    rectangle: {
+        width: '100%', 
+        backgroundColor: Colores.turquesa,
+        borderRadius: 80,
+    },
+});
+
+export default FondoAzul;
---
+++ Amicap_Expo/constants/icons.js
@@ -0,0 +1,15 @@
+import HomeSvg from "./../assets/icons/home.svg";
+import SettingsSvg from "./../assets/icons/settings.svg";
+import ProfileSvg from "./../assets/icons/profile.svg";
+import InfoSvg from "./../assets/icons/info.svg";
+import ContactSvg from "./../assets/icons/contact.svg";
+import WhiteArrowSvg from "./../assets/icons/white_arrow.svg";
+
+export const icons = {
+    home: HomeSvg,
+    settings: SettingsSvg,
+    profile: ProfileSvg,
+    info: InfoSvg,
+    contact: ContactSvg,
+    white_arrow: WhiteArrowSvg,
+  };
