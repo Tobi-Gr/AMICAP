1c59d68dfd9ce68ae384ed94a40ad62bf9a151a0
Android Studio <devandroid@ort>
2024-07-10 10:39:29 -0300

    nav bar

--- Amicap_Expo/components/FondoAzul.tsx
+++ Amicap_Expo/components/FondoAzul.tsx
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'flex-end', 
-        alignItems: 'center', 
+        alignItems: 'center'
     },
     rectangle: {
         width: '100%', 
--- Amicap_Expo/components/Navbar.tsx
+++ Amicap_Expo/components/Navbar.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import {Colores} from './../constants/Colors.ts';
-import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, StyleSheet, Dimensions} from 'react-native';
 import Home from './icons/Home.tsx';
 import Settings from './icons/Settings.tsx';
 import Profile from './icons/Profile.tsx';
 
-
-
 const Navbar = () => {
+    const windowHeight = Dimensions.get('window').height;
+    const windowWidth = Dimensions.get('window').width;
+    const barHeight = windowHeight / 11;
+    const buttonWidth = windowWidth / 3;
    return (
-     <View style={styles.container}>
-        <TouchableOpacity onPress={() => console.log('Home')}>
+     <View style={[styles.container, {height: barHeight}]}>
+        <TouchableOpacity onPress={() => console.log('Config')} style={[styles.button, { width: buttonWidth }]} activeOpacity={1} >
             <Settings color={Colores.blanco}/>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => console.log('Configuracion')}>
+        <TouchableOpacity onPress={() => console.log('Home')} style={[styles.button, { width: buttonWidth }]} activeOpacity={1} >
             <Home color={Colores.blanco}/>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => console.log('Perfil')}>
+        <TouchableOpacity onPress={() => console.log('Perfil')} style={[styles.button, { width: buttonWidth }]} activeOpacity={1} >
             <Profile color={Colores.blanco}/>
         </TouchableOpacity>
    
@@ -24,16 +26,16 @@ const Navbar = () => {
     );
   };
 
-
-  //la altura hay que calcularla basandonos en el tamaño de la pantalla
   const styles = StyleSheet.create({
     container: {
       flexDirection: 'row',
       justifyContent: 'space-around',
       alignItems: 'center',
-      height: 50,
-      backgroundColor: Colores.turquesa
+      backgroundColor: Colores.turquesa,
     },
+    button: {
+      alignItems: 'center',
+    }
   });
 
   export default Navbar;
