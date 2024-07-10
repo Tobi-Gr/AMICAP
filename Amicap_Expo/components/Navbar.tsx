40fadc518c403138412b1382a42c79667c0b5ee9
Android Studio <devandroid@ort>
2024-07-10 14:24:50 -0300

    navbar, sombra y línea

--- Amicap_Expo/components/Navbar.tsx
+++ Amicap_Expo/components/Navbar.tsx
@@ -1,15 +1,15 @@
-import React, {FC, useState} from 'react';
-import {Colores} from './../constants/Colors';
-import { View, TouchableOpacity, StyleSheet, Dimensions} from 'react-native';
+import React, { FC, useState, useRef } from 'react';
+import { Colores } from './../constants/Colors';
+import { View, TouchableOpacity, StyleSheet, Dimensions, Animated } from 'react-native';
 import Home from './icons/Home';
 import Settings from './icons/Settings';
 import Profile from './icons/Profile';
 
-interface Props{
+interface Props {
     tipo: 'home' | 'profile' | 'configuration';
 }
 
-const Navbar: FC<Props> = ({tipo}) => {
+const Navbar: FC<Props> = ({ tipo }) => {
     const windowHeight = Dimensions.get('window').height;
     const windowWidth = Dimensions.get('window').width;
     const barHeight = windowHeight / 11;
@@ -18,36 +18,60 @@ const Navbar: FC<Props> = ({tipo}) => {
     // Estado para el icono seleccionado
     const [selected, setSelected] = useState(tipo);
 
-    const getLineStyle = (icon: 'home' | 'profile' | 'configuration') => {
-        return selected === icon ? styles.selectedLine : {};
+    // Posición inicial de la línea basada en el índice del botón
+    const initialPosition = useRef(new Animated.Value(buttonWidth * getIndex()));
+
+    // Función para obtener el índice del botón seleccionado
+    function getIndex() {
+        switch (selected) {
+            case 'configuration':
+                return 0;
+            case 'home':
+                return 1;
+            case 'profile':
+                return 2;
+        }
+    }
+
+    // Función para manejar el cambio de selección
+    const handlePress = (icon: 'home' | 'profile' | 'configuration', index: number) => {
+        setSelected(icon);
+        Animated.spring(initialPosition.current, {
+            toValue: buttonWidth * index,
+            useNativeDriver: false,
+        }).start();
     };
 
     return (
-        <View style={[styles.container, {height: barHeight}]}>
-            <TouchableOpacity 
-                onPress={() => { console.log('Config'); setSelected('configuration'); }} 
-                style={[styles.button, { width: buttonWidth }]} 
+        <View style={[styles.container, { height: barHeight }]}>
+          <View style={styles.inactiveLine}></View>
+            <TouchableOpacity
+                onPress={() => handlePress('configuration', 0)}
+                style={[styles.button, { width: buttonWidth }]}
                 activeOpacity={1}
             >
-                <View style={[styles.line, getLineStyle('configuration')]} />
-                <Settings color={Colores.blanco}/>
+                <Settings color={Colores.blanco} />
             </TouchableOpacity>
-            <TouchableOpacity 
-                onPress={() => { console.log('Home'); setSelected('home'); }} 
-                style={[styles.button, { width: buttonWidth }]} 
+            <TouchableOpacity
+                onPress={() => handlePress('home', 1)}
+                style={[styles.button, { width: buttonWidth }]}
                 activeOpacity={1}
             >
-                <View style={[styles.line, getLineStyle('home')]} />
-                <Home color={Colores.blanco}/>
+                <Home color={Colores.blanco} />
             </TouchableOpacity>
-            <TouchableOpacity 
-                onPress={() => { console.log('Perfil'); setSelected('profile'); }} 
-                style={[styles.button, { width: buttonWidth }]} 
+            <TouchableOpacity
+                onPress={() => handlePress('profile', 2)}
+                style={[styles.button, { width: buttonWidth }]}
                 activeOpacity={1}
             >
-                <View style={[styles.line, getLineStyle('profile')]} />
-                <Profile color={Colores.blanco}/>
+                <Profile color={Colores.blanco} />
             </TouchableOpacity>
+            <Animated.View
+                style={[
+                    styles.line,
+                    { transform: [{ translateX: initialPosition.current }], width: buttonWidth },
+                ]}
+            />
         </View>
     );
 };
@@ -64,14 +88,20 @@ const styles = StyleSheet.create({
     },
     line: {
         position: 'absolute',
-        top: -20,
-        height: 0,
-        width: '100%',
+        top: 0,
+        height: 2,
         backgroundColor: Colores.blanco,
     },
-    selectedLine: {
-        height: 4,
+    inactiveLine: {
+      position: 'absolute',
+      top: 0,
+      height: 2,
+      backgroundColor: 'black',
+      opacity: 0.25,
+      width: '100%'
     }
 });
 
 export default Navbar;
+
+
