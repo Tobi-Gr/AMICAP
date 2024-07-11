6f747b5c8eb81b3a4523d8a20cf26dc11ae04985
Android Studio <devandroid@ort>
2024-07-11 15:54:53 -0300

    navegación

--- Amicap_Expo/app/(tabs)/home.tsx
+++ Amicap_Expo/app/(tabs)/home.tsx
@@ -1,17 +1,17 @@
-import {StyleSheet, View, Dimensions} from 'react-native';
-import {Link} from 'expo-router'
+import { StyleSheet, View, Dimensions } from 'react-native';
 import Navbar from '../../components/Navbar';
-import BotonAyuda from '../../components/BotonAyuda'
-import Add from '../../components/icons/Add'
+import BotonAyuda from '../../components/BotonAyuda';
 import FondoAzul from '@/components/FondoAzul';
-import Texto from '@/components/Texto'
-import {Colores} from '../../constants/Colors';
-import ListaContactos from '@/components/ListaContactos';
+import Texto from '@/components/Texto';
+import { Colores } from '../../constants/Colors';
 import BotonContacto from '@/components/BotonContacto';
 import BotonInfo from '@/components/BotonInfo';
 
+interface Props {
+  navigation: any;
+}
 
-export default function HomeScreen() {  
+const HomeScreen: React.FC<Props> = ({ navigation }) => {  
   const windowWidth = Dimensions.get('window').width;
   const windowHeight = Dimensions.get('window').height;
 
@@ -24,43 +24,37 @@ export default function HomeScreen() {
   const xAyuda = windowWidth / 3.5;
 
   const yContacto = windowHeight / 3;
-  const xContacto = windowWidth / 5.8
+  const xContacto = windowWidth / 5.8;
 
   const yInfo = windowHeight / 2.07;
   const xInfo = windowWidth / 12.8;
   
-  // flex:1 es para que ocupe toda la pantalla
+  const contactos = [
+    { nombre: 'Luca', numero: '5491125119535' },
+    { nombre: 'Lifschitz', numero: '9786543' }
+  ];
 
-  const contactos =
-  [
-    {nombre: 'Luca', numero: '5491125119535'},
-    {nombre: 'Lifschitz', numero: '9786543'}
-  ]
   return (
-      <View style={{flex: 1, backgroundColor: Colores.blanco}}>
-        <View style={[styles.titleContainer, {marginTop: yTexto}]}>
-          <Texto text={saludo} estilo="tituloTurquesa" style={{ fontSize: tamanoFuente }}/> 
-        </View>
-        <View style={{position:'relative'}}>
-          <FondoAzul/>
-        </View>
-        <View style={{ marginTop: yContacto, marginLeft: xContacto, position: 'absolute'}}>
-          <BotonContacto/>
-        </View> 
-        <View style={{ marginTop: yAyuda, marginLeft: xAyuda, position: 'absolute'}}>
-          <Link href="/ayuda"></Link>
-          <BotonAyuda/>
-        </View>  
-        <View style={{ marginTop: yInfo, marginLeft: xInfo, position: 'absolute'}}>
-          <BotonInfo/>
-        </View>  
-        <Navbar tipo="home"/>
+    <View style={{ flex: 1, backgroundColor: Colores.blanco }}>
+      <View style={[styles.titleContainer, { marginTop: yTexto }]}>
+        <Texto text={saludo} estilo="tituloTurquesa" style={{ fontSize: tamanoFuente }} /> 
       </View>
-        //<View style={{ marginTop: yAyuda, marginLeft: xAyuda, position: 'absolute'}}>
-          //<ListaContactos contactos={contactos}/>
-        //</View>
+      <View style={{ position: 'relative' }}>
+        <FondoAzul />
+      </View>
+      <View style={{ marginTop: yContacto, marginLeft: xContacto, position: 'absolute' }}>
+        <BotonContacto />
+      </View> 
+      <View style={{ marginTop: yAyuda, marginLeft: xAyuda, position: 'absolute' }}>
+        <BotonAyuda navigation={navigation} /> 
+      </View>  
+      <View style={{ marginTop: yInfo, marginLeft: xInfo, position: 'absolute' }}>
+        <BotonInfo />
+      </View>  
+      <Navbar tipo="home" />
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   titleContainer: {
@@ -68,3 +62,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   }
 });
+
+export default HomeScreen;
