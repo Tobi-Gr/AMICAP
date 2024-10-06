dee76e12d78996f7fd406bc9fca4c1fe348d82f6
G <tobias.granovsky@gmail.com>
2024-10-06 18:21:28 -0300

    teclado en configuración arreglado

--- Amicap_Expo/app/(tabs)/index.tsx
+++ Amicap_Expo/app/(tabs)/index.tsx
@@ -19,7 +19,7 @@ const App = () => {
   return (
     <UserProvider>
         <Stack.Navigator 
-          initialRouteName="EditarPerfil"
+          initialRouteName="Configuracion"
           screenOptions={{
             headerShown: false,
           }}>
--- Amicap_Expo/components/FondoAzul.tsx
+++ Amicap_Expo/components/FondoAzul.tsx
@@ -1,24 +1,47 @@
-import {View, StyleSheet, Dimensions} from 'react-native';
- import {Colores} from './../constants/Colors';
- const FondoAzul  = () => {
-     const windowWidth = Dimensions.get('window').width;
-     const windowHeight = Dimensions.get('window').height;
-      const rectangleHeight = windowHeight * 0.8;
-     return <View style={[styles.container, { width: windowWidth }]}>
-                 <View style={[styles.rectangle,{ height: rectangleHeight } ]} />
-             </View>;
- };
- const styles = StyleSheet.create({
-     container: {
-         flex: 1,
-         justifyContent: 'flex-end', 
-         alignItems: 'center'
-     },
-     rectangle: {
-         width: '100%', 
-         backgroundColor: Colores.turquesa,
-         borderTopLeftRadius: 80,
-         borderTopRightRadius: 80,
-     },
- });
+import {View, StyleSheet, Dimensions, Keyboard } from 'react-native';
+import { useEffect, useState } from 'react';
+import {Colores} from './../constants/Colors';
+
+const FondoAzul  = () => {
+    const windowWidth = Dimensions.get('window').width;
+    const windowHeight = Dimensions.get('window').height;
+    const [keyboardVisible, setKeyboardVisible] = useState(false);
+    useEffect(() => {
+        const keyboardDidShowListener = Keyboard.addListener(
+            'keyboardDidShow',
+            () => setKeyboardVisible(true)
+        );
+        const keyboardDidHideListener = Keyboard.addListener(
+            'keyboardDidHide',
+            () => setKeyboardVisible(false)
+        );
+        
+        return () => {
+            keyboardDidShowListener.remove();
+            keyboardDidHideListener.remove();
+        };
+    }, []);
+
+    const rectangleHeight = keyboardVisible ? windowHeight * 0.5 : windowHeight * 0.8;
+
+    return <View style={[styles.container, { width: windowWidth }]}>
+                <View style={[styles.rectangle,{ height: rectangleHeight } ]} />
+            </View>;
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        position: 'absolute',
+        alignItems: 'center',
+        top: Dimensions.get('window').height * 0.2,
+    },
+    rectangle: {
+        width: '100%', 
+        backgroundColor: Colores.turquesa,
+        borderTopLeftRadius: 80,
+        borderTopRightRadius: 80,
+    },
+});
  export default FondoAzul;
